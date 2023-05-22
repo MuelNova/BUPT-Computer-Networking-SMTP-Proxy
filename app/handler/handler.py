62a1a48d771399b064b2dc36b2d4232b00de518b
Nova-Noir <n@ova.moe>
2023-05-22 16:10:42 +0800

    :sparkles: Add Socket Parser and Handler

--- app/handler/handler.py
+++ app/handler/handler.py
@@ -1,63 +1,149 @@
 import socket
 from abc import ABC, abstractclassmethod
-from typing import Any
+from datetime import datetime
+from typing import Any, Type, Optional
 
-from ..parser import HTTPParser
+from app.config import get_config
+from app.models import HTTPParseModel, PathModel, QQMailPostModel
+from app.smtp_forwarder import get_smtp_forwarder
+from app.parser import HTTPParser
+from app.logging import logger
 
+
+config = get_config()
 
 class BaseHTTPHandler(ABC):
-    def __init__(self, parser: HTTPParser):
+    def __init__(self, parser: Type[HTTPParser]):
         if not issubclass(parser, HTTPParser):
             raise TypeError(f'parser must be a subclass of {HTTPParser.__name__}')
         self.parser = parser
 
     def __call__(self, server: socket.socket, client: socket.socket, data: bytes) -> Any:
-        type_, result = self.parser.parse(data)
-        return getattr(self, f'do_{type_}')(server, client, **result)
+        res = self.parser.parse(data)
+        return getattr(self, f'do_{res.method}')(server, client, res)
 
     @abstractclassmethod
-    def do_CONNECT(self, server: socket.socket, client: socket.socket, **kwargs: Any) -> Any:
+    def do_CONNECT(self, server: socket.socket, client: socket.socket, data: HTTPParseModel) -> Any:
         raise NotImplementedError
 
     @abstractclassmethod
-    def do_GET(self, server: socket.socket, client: socket.socket, **kwargs: Any) -> Any:
+    def do_GET(self, server: socket.socket, client: socket.socket, data: HTTPParseModel) -> Any:
         raise NotImplementedError
 
     @abstractclassmethod
-    def do_POST(self, server: socket.socket, client: socket.socket, **kwargs: Any) -> Any:
+    def do_POST(self, server: socket.socket, client: socket.socket, data: HTTPParseModel) -> Any:
         raise NotImplementedError
 
     @abstractclassmethod
-    def do_PATCH(self, server: socket.socket, client: socket.socket, **kwargs: Any) -> Any:
+    def do_PATCH(self, server: socket.socket, client: socket.socket, data: HTTPParseModel) -> Any:
         raise NotImplementedError
 
     @abstractclassmethod
-    def do_OPTION(self, server: socket.socket, client: socket.socket, **kwargs: Any) -> Any:
+    def do_OPTION(self, server: socket.socket, client: socket.socket, data: HTTPParseModel) -> Any:
         raise NotImplementedError
 
     @abstractclassmethod
-    def do_PUT(self, server: socket.socket, client: socket.socket, **kwargs: Any) -> Any:
+    def do_PUT(self, server: socket.socket, client: socket.socket, data: HTTPParseModel) -> Any:
         raise NotImplementedError
 
     @abstractclassmethod
-    def do_DELETE(self, server: socket.socket, client: socket.socket, **kwargs: Any) -> Any:
+    def do_DELETE(self, server: socket.socket, client: socket.socket, data: HTTPParseModel) -> Any:
+        raise NotImplementedError
+    
+    @abstractclassmethod
+    def make_session(self, client: socket.socket, path: PathModel) -> Optional[socket.socket]:
         raise NotImplementedError
 
 
-class SMTPProxyHandler(BaseHTTPHandler):
-    def do_CONNECT(self, server: socket.socket, client: socket.socket, **kwargs: Any) -> Any:
-        pass
+class HTTPHandler(BaseHTTPHandler):
+    def do_CONNECT(self, server: socket.socket, client: socket.socket, data: HTTPParseModel) -> Any:
+        cc = self.make_session(client, data.path)
+        if not cc:
+            return 
+        client.send(b"HTTP/1.1 200 Connection established\r\n\r\n")
+        client.setblocking(0)
+        cc.setblocking(0)
+        while True:
+            try:
+                reply = client.recv(2**13)
+                if not reply: 
+                    break
+                cc.send(reply)
+            except:
+                pass
+                
+            try:
+                reply = cc.recv(2**13)
+                if not reply:
+                    break
+                client.send(reply)
+            except:
+                pass  # We don't handle exceptions here because the lack of time
+        cc.shutdown(socket.SHUT_RDWR)
+        cc.close()
+        client.close()
+        return
+    
+    def do_GET(self, server: socket.socket, client: socket.socket, data: HTTPParseModel) -> Any:
+        return self.basic_FORWARD(server, client, data, 'GET')
+    
+    def do_POST(self, server: socket.socket, client: socket.socket, data: HTTPParseModel) -> Any:
+        return self.basic_FORWARD(server, client, data, 'POST')
+    
+    def do_PATCH(self, server: socket.socket, client: socket.socket, data: HTTPParseModel) -> Any:
+        return self.basic_FORWARD(server, client, data, 'PATCH')
+    
+    def do_OPTION(self, server: socket.socket, client: socket.socket, data: HTTPParseModel) -> Any:
+        return self.basic_FORWARD(server, client, data, 'OPTION')
+    
+    def do_PUT(self, server: socket.socket, client: socket.socket, data: HTTPParseModel) -> Any:
+        return self.basic_FORWARD(server, client, data, 'PUT')
+    
+    def do_DELETE(self, server: socket.socket, client: socket.socket, data: HTTPParseModel) -> Any:
+        return self.basic_FORWARD(server, client, data, 'DELETE')
+    
+    def make_session(self, client: socket.socket, path: PathModel) -> Optional[socket.socket]:
+        cc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            cc.connect((path.webserver, path.port))
+        except Exception as e:
+            logger.error(f'Cannot connect to {path.raw_path}, {e}')
+            cc.close()
+            client.send(b'HTTP/1.1 502 Bad Gateway\r\n\r\n')
+            client.close()
+            return None
+        return cc
+    
+    def basic_FORWARD(self, server: socket.socket, client: socket.socket, data: HTTPParseModel, method: str) -> Any:
+        cc = self.make_session(client, data.path)
+        if not cc:
+            return 
+        cc.send(data.raw)
+        while True:
+            buf = cc.recv(1024)
+            logger.debug(f'[HTTPHandler] {method} | {data.path.raw_path} | {buf}')
+            client.send(buf)
+            if not buf:
+                break
+        cc.shutdown(socket.SHUT_RDWR)
+        cc.close()
+        client.close()
+        return
+    
 
-    def do_GET(self, server: socket.socket, client: socket.socket, **kwargs: Any) -> Any:
-        pass
-
-    def do_POST(self, server: socket.socket, client: socket.socket, domain: str, **kwargs: Any) -> Any:
-        if domain == 'smtp.qq.com':
-            ...
+class SMTPProxyHandler(HTTPHandler):
+    def do_POST(self, server: socket.socket, client: socket.socket, data: HTTPParseModel) -> Any:
+        if data.path.webserver == config.QQ_MAIL_WEBSERVER and data.path.path == config.QQ_MAIL_POST_URL:
+            # 将 SMTP 交由 SMTPHandler 处理
+            param = dict(p.split('=') for p in data.body.decode('utf-8').split('&'))
+            try:
+                param = QQMailPostModel(**param, socket=client)
+            except ValueError as e:
+                client.send(b'HTTP/1.1 400 Bad Request\r\n\r\n')
+                logger.error(f'{e}')
+                client.close()
+                return
+            get_smtp_forwarder().add_forwarder(param)
         else:
-            ...
+            super().do_POST(server, client, data)
 
-    do_PATCH = do_GET
-    do_OPTION = do_GET
-    do_PUT = do_GET
-    do_DELETE = do_GET
---
+++ app/smtp_forwarder/__init__.py
@@ -0,0 +1,5 @@
+"""
+    SMTP 代理的实现，差错控制，队列等功能。
+"""
+
+from .smtp_forwarder import SMTPForwarderFactory, get_smtp_forwarder
